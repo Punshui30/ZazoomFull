10d5b2e81937f1e08ed7d0c6454bedde24e5b8d6
Punshui30 <simmonds80@gmail.com>
2025-03-08 20:15:02 -0500

    Build successful, ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-    return <h1>Hello, ZaZoom!</h1>;
+  return (
+    <div>
+      <h1>ZaZoom is Back!</h1>
+      <p>Your site is now live.</p>
+    </div>
+  );
 }
 
 export default App;
